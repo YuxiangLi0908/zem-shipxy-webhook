70bf7e9e59c39fd56e962ab370e339bd95c1b5a9
yuxiang <yuxiangli.0908@gmail.com>
2025-11-17 22:49:18 -0500

    initial commit

---
+++ startup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export POETRY_HOME="/opt/poetry"
+export PATH="$POETRY_HOME/bin:$PATH"
+
+# Install poetry if not installed
+if [ ! -d "$POETRY_HOME" ]; then
+    curl -sSL https://install.python-poetry.org | python3 -
+fi
+
+cd /home/site/wwwroot
+
+# Install dependencies
+poetry install --no-interaction --no-root
+
+# Run FastAPI
+poetry run uvicorn app.main:app --host 0.0.0.0 --port 8000
