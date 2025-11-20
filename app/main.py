cccf4218a2776767fc228e312d21975d69a5ac48
yuxiang <yuxiangli.0908@gmail.com>
2025-11-20 00:20:30 -0500

    set up api

--- app/main.py
+++ app/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
+
+from app.api.router import api_router
+
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
+app.add_middleware(GZipMiddleware)
 
 @app.get("/")
 def read_root():
     return {"message": "Hello from FastAPI on Azure App Service!"}
+
+app.include_router(api_router)
