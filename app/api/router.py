1b1abbf84685f60c30d9e371496f4d07950adc2c
yuxiang <yuxiangli.0908@gmail.com>
2025-11-26 22:29:38 -0500

    add shipeta endpoint

--- app/api/router.py
+++ app/api/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
-from app.api import heartbeat, ship_added
+from app.api import heartbeat, fleet_ship_eta
 
 api_router = APIRouter()
 api_router.include_router(heartbeat.router, tags=["health"])
-api_router.include_router(ship_added.router, tags=["ship_added"])
+api_router.include_router(fleet_ship_eta.router, tags=["fleet_ship_eta"])
